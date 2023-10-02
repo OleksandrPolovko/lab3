ec6b19349d3c7727625bc0d94bef09e64e01cfd3
OleksandrPolovko <O.Polovko_FIT_12_22_B_d@knute.edu.ua>
2023-10-02 21:12:00 +0300

    Figure

--- Program.cs
+++ Program.cs
@@ -24,10 +24,44 @@ namespace Lab3
             Console.WriteLine("Периметер прямокутника: " + rect.CalculatePerimeter());
         }
 
+        static void Task2()
+        {
+            Console.Write("Назва фігури: ");
+            string name = Console.ReadLine();
+            Console.Write("З скількох точок складається фігура (3-5): ");
+            string sNum = Console.ReadLine();
+            int num = Convert.ToInt32(sNum);
+            if (num<3 || num>5)
+            {
+                Console.Write("Неправильне значення");
+                return;
+            }
+            Point[] points = new Point[num];
+            for (int i=0;i<num;i++)
+            {
+                Console.Write("Точка" + i + " X=");
+                string sX = Console.ReadLine();
+                double x = Convert.ToDouble(sX);
+                Console.Write("Точка" + i + " Y=");
+                string sY = Console.ReadLine();
+                double y = Convert.ToDouble(sY);
+                points[i] = new Point(x, y);
+            }
+            Figure f;
+            if (num == 3)
+                f = new Figure(name, points[0], points[1], points[2]);
+            else if (num == 4)
+                f = new Figure(name, points[0], points[1], points[2], points[3]);
+            else
+                f = new Figure(name, points[0], points[1], points[2], points[3], points[4]);
 
+            Console.WriteLine("Фігура " + f.Title + " має периметер " + f.Perimeter);
+        }
         static void Main(string[] args)
         {
-            Task1();
+            //Task1();
+            Task2();
+            Console.Read();
         }
     }
 }
