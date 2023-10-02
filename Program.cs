c89dfcc826fc3ce57a2283d5a9c9dea741a8d3cb
OleksandrPolovko <O.Polovko_FIT_12_22_B_d@knute.edu.ua>
2023-10-02 19:48:47 +0300

    Rectangle

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Program
+    {
+
+        static void Task1()
+        {
+            Console.WriteLine("Введіть першу довжину прямокутника:");
+            string strA = Console.ReadLine();
+            double A = Convert.ToDouble(strA);
+
+            Console.WriteLine("Введіть другу довжину прямокутника:");
+            string strB = Console.ReadLine();
+            double B = Convert.ToDouble(strB);
+
+            Rectangle rect = new Rectangle(A, B);
+            Console.WriteLine("Площа прямокутника: " + rect.Area);
+            Console.WriteLine("Периметер прямокутника: " + rect.CalculatePerimeter());
+        }
+
+
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    //Створити клас із ім'ям Rectangle.
+    internal class Rectangle
+    {
+        //У тілі класу створити два поля, що описують довжини сторін double side1, side2
+        private double side1;
+        private double side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            // в тілі якого поля side1 та side2 ініціалізуються значеннями аргументів.
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        //обчислюють площу прямокутника
+        public double CalculateArea()
+        {
+            return side1 * side2;            
+        }
+
+        //периметр прямокутника
+        public double CalculatePerimeter()
+        {
+            return side1 + side2;
+        }
+
+        public double Area
+        { get { return CalculateArea(); } }
+
+        public double Perimeter
+        { get { return CalculatePerimeter(); } }
+
+    }
+}
